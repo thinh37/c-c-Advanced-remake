b066d1907a4bd2e3630a0c1f2fa5c6840dcd0891
thinh37 <109522214+thinh37@users.noreply.github.com>
2023-03-31 22:39:21 +0700

    Add files via upload

---
+++ Convert_String_to_Bin/source/String_to_Bin.c
@@ -0,0 +1,23 @@
+/*
+* File: linked.h
+* Author: Quach Minh Thinh
+* Date: 30/03/2023
+* Description: This file contains all the functions for the String to Binary.
+*/
+#include "String_to_Bin.h"
+void convertStringtoBin(char *a){
+int i,j;
+for( i=0;a[i]!='\0';i++){
+  char Binary[BYTE];
+  int t=a[i];
+  for(j=BYTE-1;j>=0;j--){
+    if(t%2==0){
+      Binary[j]=Bit_0;
+    }else{
+      Binary[j]=Bit_1;
+    }
+    t=t/2;
+  }
+  printf("%c: %s\n",a[i],Binary);
+}
+}
---
+++ Convert_String_to_Bin/source/main.c
@@ -0,0 +1,6 @@
+#include "String_to_Bin.h"
+
+int main(void)
+{
+  convertStringtoBin("my name is thinh");
+}
