c9606a5adec1f73cc15d91ad4bc3cf63449b326f
thinh37 <109522214+thinh37@users.noreply.github.com>
2023-04-01 19:02:09 +0700

    Update String_to_Bin.h

--- Convert_String_to_Bin/header/String_to_Bin.h
+++ Convert_String_to_Bin/header/String_to_Bin.h
@@ -1,7 +1,7 @@
 /*
-* File: linked.h
+* File: String_to_Bin.h
 * Author: Quach Minh Thinh
-* Date: 30/03/2023
+* Date: 31/03/2023
 * Description: This is a header file for String to Binary
 */
 #ifndef __STRING_TO_BIN_H
